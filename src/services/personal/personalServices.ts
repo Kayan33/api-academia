1e602892b679f5352c69874600d238f31d4cae26
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2024-10-17 16:37:23 -0300

    Implementação da função ver_personal com seleção de campos

--- src/services/personal/personalServices.ts
+++ src/services/personal/personalServices.ts
@@ -1,28 +1,44 @@
 import prismaClient from "../prisma";
 
 interface Personal {
-  nome:string,
-  telefone:string,
-  email:string,
-  CREF:string,
-  sexo:string,
-  senha:string
+  nome: string,
+  telefone: string,
+  email: string,
+  CREF: string,
+  sexo: string,
+  senha: string
 }
-class PersonalServices{
-async cadastrar_personal({nome,telefone,email,CREF,sexo,senha}:Personal){
-  const cadastrar = await prismaClient.personal.create({
-    data:{
-      nome,
-      telefone,
-      email,
-      CREF,
-      sexo,
-      senha
-    }
-  })
-  return cadastrar
-}
+class PersonalServices {
+
+  async cadastrar_personal({ nome, telefone, email, CREF, sexo, senha }: Personal) {
+    const cadastrar = await prismaClient.personal.create({
+      data: {
+        nome,
+        telefone,
+        email,
+        CREF,
+        sexo,
+        senha
+      }
+    })
+    return cadastrar
+  }
+
+  async getAllPersonalTrainers() {
+    const ver = await prismaClient.personal.findMany({
+      select: {
+        id: true,
+        nome:true,
+        telefone:true,
+        email:true,
+        CREF:true,
+        sexo:true,
+        senha:true,
+      }
+    })
+    return ver
+  }
 }
 
-export {PersonalServices}
+export { PersonalServices }
 
