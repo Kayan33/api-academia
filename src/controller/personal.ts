2334aa08d5ec2c3967eac99a1595ee370d9ece91
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2024-10-17 16:38:42 -0300

    Implementa método para buscar todos os personal trainers

--- src/controller/personal.ts
+++ src/controller/personal.ts
@@ -9,7 +9,14 @@ class PersonalController {
         const resposta = await personalServices.cadastrar_personal({
             nome,telefone,email,CREF,sexo,senha
         })
-        return resposta
+        return res.json(resposta)
+    }
+
+    async getPersonalTrainers(req:Request,res:Response) {
+        const personalServices = new PersonalServices()
+        const resposta = await personalServices.getAllPersonalTrainers()
+        
+        return res.json(resposta)
     }
 }
 
