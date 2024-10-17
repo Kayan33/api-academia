6000f4439fc9ad34d2e7af7c9d7fccbe531b23ac
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2024-10-17 14:58:46 -0300

    Adiciona rota POST para cadastro de Personal

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import PersonalController from "./controller/personal";
 
 const router = Router()
+
+router.post('/personal', new PersonalController().cadastroPersonal)
 
 export default router
