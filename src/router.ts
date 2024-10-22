c5b48630daa752a7b483523c1a175c87775513fb
Kayan33 <kayanpereira33@gmail.com>
2024-10-22 12:09:09 -0300

    feat: adicionar rota '/rotina' para cadastro de rotinas no roteador

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import PersonalController from "./controller/personal/personal";
 import TreinoController from "./controller/treino/treino";
 import ExercicioController from "./controller/exercicio/exercicio";
 import AlunoController from "./controller/aluno/aluno";
+import RotinaController from "./controller/rotina/rotina";
 
 const router = Router()
 
@@ -14,6 +15,8 @@ router.get('/personal', new PersonalController().getPersonalTrainers)
 
 router.post('/treino', new TreinoController().cadastro_Treino)
 router.get('/treino', new TreinoController().getTreino)
+
+router.post('/rotina', new RotinaController().cadastro_Rotina)
 
 router.post('/exercicio', new ExercicioController().cadastro_Exercicio)
 export default router
