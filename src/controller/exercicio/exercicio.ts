b2c672eaa7ba3dca961f97210258e2f1a9a2d834
Kayan33 <kayanpereira33@gmail.com>
2024-10-22 10:45:13 -0300

    style: ajustar formatação no controlador de exercício

--- src/controller/exercicio/exercicio.ts
+++ src/controller/exercicio/exercicio.ts
@@ -1,11 +1,9 @@
 import { Request, Response } from "express";
 import ExercicioServices from "../../services/exercicio/exercicioServices";
 
-class ExercicioController{
-  async cadastro_Exercicio(req:Request, res:Response){
-    const {nome_exercicio,
-      descricao,
-      URL_video,} = req.body
+class ExercicioController {
+  async cadastro_Exercicio(req: Request, res: Response) {
+    const { nome_exercicio, descricao, URL_video, } = req.body
     const exercicioServices = new ExercicioServices()
     const resposta = await exercicioServices.cadastrar_exercicios({
       nome_exercicio,
