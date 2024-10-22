42764aa69d5dcf9a8802f095db26e1518f8a71b0
Kayan33 <kayanpereira33@gmail.com>
2024-10-21 21:28:14 -0300

    ajustes na para pasta controller\personal\personal.ts

--- src/controller/personal/personal.ts
+++ src/controller/personal/personal.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
-import { PersonalServices } from "../services/personal/personalServices";
+import { PersonalServices } from "../../services/personal/personalServices";
 
 
 class PersonalController {
-    async cadastroPersonal(req:Request, res:Response){
+    async cadastro_Personal(req:Request, res:Response){
         const {nome,telefone,email,CREF,sexo,senha} = req.body;
         const personalServices = new PersonalServices()
         const resposta = await personalServices.cadastrar_personal({
