fae65f99608224ffec97b1aeb00cc6fe202283f7
Kayan33 <kayanpereira33@gmail.com>
2024-10-22 10:43:42 -0300

    feat: adicionar controlador para cadastro de alunos

---
+++ src/controller/aluno/aluno.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import AlunoServises from "../../services/aluno/alunoServices";
+
+
+class AlunoController {
+ async cadastro_Aluno (req:Request, res:Response){
+  const {nome,telefone,email,endereco,data_nascimento,status}= req.body
+  const alunoServices = new AlunoServises()
+  const resposta =  await alunoServices.Cadastar_Aluno({
+    nome,
+    telefone,
+    email,
+    endereco,
+    data_nascimento,
+    status
+  })
+return res.json(resposta)
+ }
+}
+export default AlunoController
