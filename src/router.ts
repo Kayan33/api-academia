5c62c19526cb74d4c010bd2edb4134b0e8028f1d
Kayan33 <kayanpereira33@gmail.com>
2024-10-21 21:26:28 -0300

    atualizar rotas para incluir treinadores, treinos e exercícios

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import PersonalController from "./controller/personal";
+import PersonalController from "./controller/personal/personal";
+import TreinoController from "./controller/treino/treino";
+import ExercicioController from "./controller/exercicio/exercicio";
 
 const router = Router()
 
-router.post('/personal', new PersonalController().cadastroPersonal)
+router.post('/personal', new PersonalController().cadastro_Personal)
 router.get('/personal', new PersonalController().getPersonalTrainers)
 
+
+router.post('/treino', new TreinoController().cadastro_Treino)
+router.get('/treino', new TreinoController().getTreino)
+
+router.post('/exercicio', new ExercicioController().cadastro_Exercicio)
 export default router
