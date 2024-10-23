f4bbcbecb6dcee8015da4822c008caa12b6bf00d
Kayan33 <kayanpereira33@gmail.com>
2024-10-23 13:05:00 -0300

    refactor: atualizar cadastro de rotina para suportar múltiplos exercícios

--- src/controller/rotina/rotina.ts
+++ src/controller/rotina/rotina.ts
@@ -3,14 +3,12 @@ import RotinaServices from "../../services/rotina/rotinaServices";
 
 class RotinaController {
 async cadastro_Rotina (req:Request, res:Response){
-  const {dia_semana,
-       descricao,
-       id_exercicio}=req.body
+  const {dia_semana,descricao,exercicio}=req.body
   const rotinaService = new RotinaServices()
   const resposta = await rotinaService.cadastar_Rotina({
     dia_semana,
        descricao,
-       id_exercicio
+       exercicio
   })
 return res.json(resposta)
 }
