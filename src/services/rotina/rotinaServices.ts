1f321f8718015d6aa9c041cc4c0227bb3df6c6b1
Kayan33 <kayanpereira33@gmail.com>
2024-10-23 13:03:34 -0300

    feat: adicionar suporte para múltiplos exercícios no cadastro de rotina

--- src/services/rotina/rotinaServices.ts
+++ src/services/rotina/rotinaServices.ts
@@ -3,16 +3,18 @@ import prismaClient from "../prisma";
 interface Rotina {
   dia_semana: string
   descricao : string
-  id_exercicio : string
+  exercicio : string[]
 }
 
 class RotinaServices{
- async cadastar_Rotina ({dia_semana,descricao,id_exercicio}:Rotina){
+ async cadastar_Rotina ({dia_semana,descricao,exercicio}:Rotina){
   const cadastrar = await prismaClient.rotina.create({
     data:{
       dia_semana,
        descricao,
-       id_exercicio
+       exercicio:{
+        connect: exercicio.map(id=>({id}))
+       }
     },
     include:{
       exercicio:true
