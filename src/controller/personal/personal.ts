60bc95aa0458ea7a246e98f9b6b66fce20fa4eaa
Kayan33 <kayanpereira33@gmail.com>
2024-12-02 13:01:08 -0300

    personal

--- src/controller/personal/personal.ts
+++ src/controller/personal/personal.ts
@@ -4,10 +4,10 @@ import { PersonalServices } from "../../services/personal/personalServices";
 
 class PersonalController {
     async cadastro_Personal(req:Request, res:Response){
-        const {nome,telefone,email,CREF,sexo,senha,alunoId} = req.body;
+        const {nome,telefone,email,CREF,sexo,senha,aluno} = req.body;
         const personalServices = new PersonalServices()
         const resposta = await personalServices.cadastrar_personal({
-            nome,telefone,email,CREF,sexo,senha,alunoId
+            nome,telefone,email,CREF,sexo,senha,aluno
         })
         return res.json(resposta)
     }
@@ -16,6 +16,29 @@ class PersonalController {
         const personalServices = new PersonalServices()
         const resposta = await personalServices.getAllPersonalTrainers()
         
+        return res.json(resposta)
+    }
+
+    async consultarUsuariosUnico(req:Request,res:Response){
+        const {id}= req.params
+        const personalServices = new PersonalServices()
+        const resposta = await personalServices.consultarPersonalUnico(id)
+        return res.json(resposta)
+    }
+
+    async alterarDadosPersonal(req:Request,res:Response){
+        const{id}=req.params
+        const {nome,telefone,email,CREF,sexo,aluno} = req.body;
+        const personalServices = new PersonalServices()
+        const resposta = await personalServices.alterarDadosPersonal({
+            id,
+            nome,
+            telefone,
+            email,
+            CREF,
+            sexo,
+            aluno
+        })
         return res.json(resposta)
     }
 }
--- src/services/personal/personalServices.ts
+++ src/services/personal/personalServices.ts
@@ -8,12 +8,23 @@ interface Personal {
   CREF: string,
   sexo: string,
   senha: string,
-  alunoId: string[] 
+  aluno: string[]
 }
 
+interface AlterarPersonal {
+  id: string
+  nome: string,
+  telefone: string,
+  email: string,
+  CREF: string,
+  sexo: string,
+  aluno: string[]
+}
+
+
 class PersonalServices {
-  async cadastrar_personal({ nome, telefone, email, CREF, sexo, senha, alunoId}: Personal) {
-    
+  async cadastrar_personal({ nome, telefone, email, CREF, sexo, senha, aluno }: Personal) {
+
     const senhaCrypt = await hash(senha, 8)
     const cadastrar = await prismaClient.personal.create({
       data: {
@@ -22,9 +33,9 @@ class PersonalServices {
         email,
         CREF,
         sexo,
-        senha:senhaCrypt,
-        
-        aluno: alunoId.length > 0 ? { connect: alunoId.map(id => ({ id })) } : undefined,
+        senha: senhaCrypt,
+
+        aluno: aluno.length > 0 ? { connect: aluno.map(id => ({ id })) } : undefined,
       },
       include: {
         aluno: true,
@@ -36,10 +47,45 @@ class PersonalServices {
   async getAllPersonalTrainers() {
     const ver = await prismaClient.personal.findMany({
       include: {
-        aluno: true 
+        aluno: true
       }
     });
     return ver;
+  }
+
+  // ERRO AO BUSCAR COM findFirst PELO FRONT
+  async consultarPersonalUnico(id: string) {
+    const resposta = await prismaClient.personal.findMany({
+      where: {
+        id: id
+      },
+      include: {
+        aluno: true
+      }
+    })
+   
+
+    return resposta
+  }
+
+  async alterarDadosPersonal({ id, nome, telefone, email, CREF, sexo, aluno }: AlterarPersonal) {
+    await prismaClient.personal.update({
+      where: {
+        id: id
+      },
+      data: {
+        nome: nome,
+        telefone: telefone,
+        email: email,
+        CREF: CREF,
+        sexo: sexo,
+        aluno: aluno.length > 0 ? { connect: aluno.map(id => ({ id })) } : undefined,
+      },
+      include: {
+        aluno: true
+      }
+    })
+    return ({dados:'Cadastro Efetuado com Sucesso'})
   }
 }
 
