502174928841d425f4cb9750efda9cc70bc3fa23
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2025-02-11 14:45:37 -0300

    refatoração

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,42 @@
-import Express, { Response, Request, NextFunction } from "express";
-import 'express-async-errors'
-import cors from 'cors'
-import router from "./router";
+
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import router from './router';
 import swaggerUi from "swagger-ui-express";
 import swaggerDoc from "./swagger.json"
-const app = Express();
+import * as https from 'https';
+import * as fs from 'fs';
+import path from 'path';
 
-app.use(Express.json())
-app.use(cors())
-app.use(router)
-app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDoc))
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-if (err instanceof Error) {
-    return res.status(400).json({
-        error:err.message
-    })
+const options = {
+    key: fs.readFileSync(path.resolve(__dirname, '..',  'cert/certificadokey.key')),
+    cert: fs.readFileSync(path.resolve(__dirname, '..', 'cert/certificadocert.crt')),
+    ca: fs.readFileSync(path.resolve(__dirname, '..', 'cert/ca.crt')),
 }
-return res.status(500).json({
-    status:'Erro',
-    message:'Erro interno do servidor'
-})
-})
 
-app.listen(7777,()=>console.log('servidor ON LINE'))
-export default app
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDoc))
+ 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);  
+    if(err instanceof Error){
+        return res.status(400).json({
+            message: err.message
+        });
+    }
+ 
+    return res.status(500).json({
+        'err': 'Erro',
+        'message':'Erro interno no servidor'
+    });
+});
+ 
+//app.listen(7777, () => console.log("Rodando no http://localhost:7777/"));
+ 
+https.createServer(options, app).listen(3000, () => console.log('Servidor OnLine'))
